182a70be40d4cb7b3dd5f91e4357718841537144
Inma Gijón <igijoninf@gmail.com>
2022-02-22 13:15:08 +0100

    Lazy load

--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,26 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 import ListPage from '@/modules/pokemon/pages/ListPage'
-import AboutPage from '@/modules/pokemon/pages/AboutPage'
 import PokemonPage from '@/modules/pokemon/pages/PokemonPage'
 import NoPageFound from '@/modules/shared/pages/NoPageFound'
 
 const routes = [
-    {path: '/', component: ListPage},
-    {path: '/about', component: AboutPage},
-    {path: '/id', component: PokemonPage},
-    {path: '/:pathMatch(.*)*', component: NoPageFound}, //Cualquier URL que no haga match con las anteriores mostraré un 404Page
+    {
+        path: '/', 
+        component: ListPage
+    },
+    {
+        path: '/about', 
+        component: () => import('../modules/pokemon/pages/AboutPage') //Lazy Load
+    },
+    {
+        path: '/id', 
+        component: PokemonPage
+    },
+    {
+        path: '/:pathMatch(.*)*', 
+        component: NoPageFound
+    }, //Cualquier URL que no haga match con las anteriores mostraré un 404Page
 
 ]
 
