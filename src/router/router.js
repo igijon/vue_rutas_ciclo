9c593bb7794b5ee10ddae7cf3c9c84b014010720
Inma Gijón <igijoninf@gmail.com>
2022-02-22 13:09:23 +0100

    No Page Found

--- src/router/router.js
+++ src/router/router.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import ListPage from '@/modules/pokemon/pages/ListPage'
 import AboutPage from '@/modules/pokemon/pages/AboutPage'
 import PokemonPage from '@/modules/pokemon/pages/PokemonPage'
+import NoPageFound from '@/modules/shared/pages/NoPageFound'
 
 const routes = [
     {path: '/', component: ListPage},
     {path: '/about', component: AboutPage},
     {path: '/id', component: PokemonPage},
+    {path: '/:pathMatch(.*)*', component: NoPageFound}, //Cualquier URL que no haga match con las anteriores mostraré un 404Page
 
 ]
 
