fe7c1c23fc87df9dd6e55b040335140df9beb6c3
Inma Gijón <igijoninf@gmail.com>
2022-02-23 14:22:38 +0100

    Estilos navbar

--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -3,6 +3,7 @@
       <a href="/">Pokemon List</a>
       <a href="/id">Pokemon por id</a>
       <a href="/about">About</a>
+      <!--Aún no funciona y recarga todo ... paciencia-->
   </div>
 </template>
 
@@ -13,5 +14,15 @@ export default {
 </script>
 
 <style scoped>
+
+div {
+    padding: 0 30px;
+}
+
+div a {
+    font-weight: bold;
+    color: #2c3e50;
+    margin: 0 10px;
+}
 
 </style>
