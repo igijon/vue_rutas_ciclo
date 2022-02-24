96c5bed473d923bd730f261628ddfc7d7c7c690d
Inma Gijón <igijoninf@gmail.com>
2022-02-24 21:20:58 +0100

    Explicación

--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -3,7 +3,12 @@
       <router-link to="/">Pokemon List</router-link>
       <router-link to="/id">Pokemon por id</router-link>
       <router-link to="/about">About</router-link>
-      <!--Con vue router no usa a, usa router-link-->
+      <!--Con vue router no usa a, usa router-link
+      aunque en el navegador web lo termina traduciendo a "a"
+      Para que las clases de estilos sólo se apliquen a los elementos
+      de los componentes, cuando se renderiza (en el html lo podemos ver)
+      se les asigna un hash para que no se apliquen a todo. Si quitamos el
+      scoped sí se aplica a todo.-->
   </div>
 </template>
 
