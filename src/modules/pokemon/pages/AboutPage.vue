59e2bdaadaf4c492f4fa016195364626706a1b68
Inma Gijón <igijoninf@gmail.com>
2022-02-24 21:45:48 +0100

    Hooks de options API

--- src/modules/pokemon/pages/AboutPage.vue
+++ src/modules/pokemon/pages/AboutPage.vue
@@ -1,3 +1,26 @@
 <template>
+    <!--
+        https://vuejs.org/api/composition-api-lifecycle.html
+        https://vuejs.org/guide/essentials/application.html#lifecycle-diagram
+    -->
     <h1>About Page</h1>
 </template>
+
+<script>
+
+export default {
+    beforeCreate() {},
+    created() {},
+    beforeMount() {},
+    mounted() {},
+    beforeUpdate() {},
+    updated() {},
+    activated() {},
+    deactivated() {},
+    beforeUnmount() {},
+    unmounted() {},
+    errorCaptured() {},
+    renderTracked() {},
+    renderTriggered() {}
+}
+</script>
