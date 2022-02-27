5fce13886b074decb240cb78572a37576a556010
Inma Gijón <igijoninf@gmail.com>
2022-02-27 18:25:42 +0100

    Preparando para que id sea una propiedad

--- src/modules/pokemon/pages/PokemonPage.vue
+++ src/modules/pokemon/pages/PokemonPage.vue
@@ -4,15 +4,23 @@
 <script>
 
 export default {
+    props: {
+        //Vamos a ver la alternativa de tener id como propiedad en el componente en lugar
+        //de como parámetro.
+        id: {
+            type: Number,
+            required: true
+        }
+    },
     data(){
         return {
-            id: null
+            //id: null
         }
     },
     created(){
         const { id } = this.$route.params
         console.log(id)
-        this.id = id
+        //this.id = id
     }
 }
 </script>
