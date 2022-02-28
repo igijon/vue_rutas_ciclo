e754ca132cb6264aa0f3b31f28ff2920b33f129f
Inma Gijón <igijoninf@gmail.com>
2022-02-28 10:42:57 +0100

    Rutas hijas - path absoluto

--- src/router/router.js
+++ src/router/router.js
@@ -10,29 +10,32 @@ const routes = [
     {
         path: '/pokemon',
         name: 'pokemon',
-        component: () => import(/* webpackChunkName: "PokemonLayout"*/ '@/modules/pokemon/layouts/PokemonLayout')
-    },
-    // {
-    //     path: '/home', 
-    //     name: 'home',
-    //     component: () => import(/* webpackChunkName: "ListPage"*/ '../modules/pokemon/pages/ListPage') //Lazy Load
-    // },
-    // {
-    //     path: '/about',
-    //     name: 'about', 
-    //     component: () => import(/* webpackChunkName: "AboutPage"*/ '../modules/pokemon/pages/AboutPage') //Lazy Load
-    // },
-    // {
-        
-    //     path: '/pokemonid/:id', 
-    //     name: 'pokemon-id', 
-    //     component: () => import(/* webpackChunkName: "PokemonPage"*/ '../modules/pokemon/pages/PokemonPage'), //Lazy Load
-    //     props: ( route ) => {
-    //         const id  = Number(route.params.id)
-    //         return isNaN( id ) ? { id: 1 } : { id }
+        component: () => import(/* webpackChunkName: "PokemonLayout"*/ '@/modules/pokemon/layouts/PokemonLayout'),
+        children: [
+            /**Se comporta igual porque las rutas hijas tienen el path absoluto */
+            {
+                path: '/home', 
+                name: 'home',
+                component: () => import(/* webpackChunkName: "ListPage"*/ '../modules/pokemon/pages/ListPage') //Lazy Load
+            },
+            {
+                path: '/about',
+                name: 'about', 
+                component: () => import(/* webpackChunkName: "AboutPage"*/ '../modules/pokemon/pages/AboutPage') //Lazy Load
+            },
+            {
                 
-    //     } 
-    // },
+                path: '/pokemonid/:id', 
+                name: 'pokemon-id', 
+                component: () => import(/* webpackChunkName: "PokemonPage"*/ '../modules/pokemon/pages/PokemonPage'), //Lazy Load
+                props: ( route ) => {
+                    const id  = Number(route.params.id)
+                    return isNaN( id ) ? { id: 1 } : { id }
+                        
+                } 
+            },
+        ]
+    },
     {
         path: '/:pathMatch(.*)*', 
         //component: () => import(/* webpackChunkName: "NoPageFound"*/ '../modules/shared/pages/NoPageFound') //Lazy Load
